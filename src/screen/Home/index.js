4d2037257f4161fc451d327f2748c2fd62e1bbab
AhmadZaky19 <a.zaky32@gmail.com>
2021-12-03 05:40:49 +0700

    add slicing home screen (50%)

--- src/screen/Home/index.js
+++ src/screen/Home/index.js
@@ -1,31 +1,119 @@
 import React, {useEffect} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, ScrollView, Image, FlatList} from 'react-native';
+import {Input, Button} from 'react-native-elements';
 import styles from './style';
-import Icon from 'react-native-vector-icons/FontAwesome';
 
 function Home(props) {
-  // useEffect(() => {
-  //   console.log(props.route.params.nama);
-  // });
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.header}>
-        <View style={{backgroundColor: 'yellow', flex: 1}}>
-          <Text>Left Side</Text>
+        <Text style={styles.header1}>Nearest Cinema, Newest Movie,</Text>
+        <Text style={styles.header2}>Find out now!</Text>
+      </View>
+      <View style={styles.jumbotron}>
+        <Image
+          source={require('../../assets/img/groupMovie.png')}
+          style={styles.jumbotronImage}
+        />
+      </View>
+      <View style={styles.movieList}>
+        <View style={styles.movieListHeader}>
+          <Text style={styles.movieListHeaderLeft}>Now Showing</Text>
+          <Text style={styles.movieListHeaderRight}>view all</Text>
         </View>
-        <View style={{backgroundColor: 'red', flex: 1}}>
-          <Text>Left Side</Text>
+        <View>
+          <FlatList />
         </View>
       </View>
-      <View style={styles.main}>
-        <Text>Main</Text>
-        <Icon name="facebook" size={35} color="blue" />
+      <View style={styles.movieList}>
+        <View style={styles.movieListHeader}>
+          <Text style={styles.movieListHeaderLeft}>Upcoming Movies</Text>
+          <Text style={styles.movieListHeaderRight}>view all</Text>
+        </View>
+        <View>
+          <FlatList />
+        </View>
       </View>
+      <View>
+        <View style={styles.join}>
+          <View style={styles.joinTextTop}>
+            <Text style={styles.joinTextTop1}>Be the vanguard of the</Text>
+            <Text style={styles.joinTextTop2}>Moviegoers</Text>
+          </View>
+          <Input
+            placeholder="Type your email"
+            keyboardType="email-address"
+            inputContainerStyle={styles.input}
+          />
+          <Button title="Join now" buttonStyle={styles.button} />
+          <Text style={styles.joinTextBottom}>
+            By joining you as a Tickitz member, {'\n'} we will always send you
+            the {'\n'} latest updates via email.
+          </Text>
+        </View>
+      </View>
+      {/* ---------------------- */}
       <View style={styles.footer}>
-        <Text>Footer</Text>
+        <Image
+          source={require('../../assets/img/tickitzPurple.png')}
+          style={styles.logoImage}
+        />
+        <Text style={styles.footerText}>
+          Stop waiting in line. Buy tickets {'\n'}conveniently, watch movies
+          quietly.
+        </Text>
+        <View style={styles.footerSection}>
+          <Text style={styles.sectionHeader}>Explore</Text>
+          <View style={styles.sectionItem}>
+            <Text>Cinemas</Text>
+            <Text>Movies List</Text>
+            <Text>Notification</Text>
+          </View>
+          <Text style={styles.sectionItem}>My Ticket</Text>
+        </View>
+        <View style={styles.footerSection}>
+          <Text style={styles.sectionHeader}>Our Sponsor</Text>
+          <View style={styles.sectionItem}>
+            <Image
+              source={require('../../assets/img/ebuid.png')}
+              style={styles.sectionLogo1}
+            />
+            <Image
+              source={require('../../assets/img/cineone.png')}
+              style={styles.sectionLogo2}
+            />
+            <Image
+              source={require('../../assets/img/hiflix.png')}
+              style={styles.sectionLogo3}
+            />
+          </View>
+        </View>
+        <View style={styles.footerSection}>
+          <Text style={styles.sectionHeader}>Follow us</Text>
+          <View style={styles.sectionVector}>
+            <Image
+              source={require('../../assets/img/v-facebook.png')}
+              style={styles.sectionItemVector}
+            />
+            <Image
+              source={require('../../assets/img/v-instagram.png')}
+              style={styles.sectionItemVector}
+            />
+            <Image
+              source={require('../../assets/img/v-twitter.png')}
+              style={styles.sectionItemVector}
+            />
+            <Image
+              source={require('../../assets/img/v-youtube.png')}
+              style={styles.sectionItemVector}
+            />
+          </View>
+        </View>
+        <Text style={styles.footerEnd}>
+          © 2020 Tickitz. All Rights Reserved.
+        </Text>
       </View>
-      {/* <Text style={styles.textMedium}>Home Screen</Text> */}
-    </View>
+    </ScrollView>
   );
 }
 
--- src/screen/Home/style.js
+++ src/screen/Home/style.js
@@ -1,31 +1,146 @@
 import {StyleSheet} from 'react-native';
+import * as color from '../../styles/colorStyles';
 
 export default StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'blue',
-    margin: 10,
+    backgroundColor: color.white,
   },
   header: {
-    flex: 1,
-    width: '100%',
-    flexDirection: 'row',
-    backgroundColor: 'red',
+    marginTop: 40,
+    marginBottom: 20,
+    marginHorizontal: 25,
   },
-  main: {
-    flex: 2,
-    width: '100%',
-    backgroundColor: 'green',
+  header1: {
+    fontSize: 15,
+    color: color.placeholder,
   },
-  footer: {
-    flex: 1,
-    width: '100%',
-    backgroundColor: 'grey',
-  },
-  textMedium: {
+  header2: {
     fontSize: 40,
-    color: 'black ',
+    color: color.primary,
+    fontWeight: '700',
+  },
+  jumbotron: {
+    alignItems: 'center',
+  },
+  jumbotronImage: {
+    resizeMode: 'contain',
+    width: 400,
+  },
+  movieList: {
+    marginVertical: 30,
+    marginHorizontal: 25,
+  },
+  movieListHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  movieListHeaderLeft: {
+    fontSize: 18,
+    fontWeight: '700',
+    color: color.textTitle,
+  },
+  movieListHeaderRight: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: color.primary,
+  },
+  join: {
+    marginVertical: 40,
+    marginHorizontal: 20,
+    paddingVertical: 30,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: 'rgba(186, 186, 186, 0.15)',
+  },
+  joinTextTop: {
+    alignItems: 'center',
+  },
+  joinTextTop1: {
+    fontSize: 14,
+    fontWeight: '400',
+    color: color.textBody1,
+  },
+  joinTextTop2: {
+    fontSize: 32,
+    fontWeight: '700',
+    color: color.primary,
+  },
+  input: {
+    borderColor: color.border,
+    borderWidth: 1,
+    backgroundColor: color.inputBg,
+    borderBottomWidth: 0,
+    borderRadius: 4,
+    paddingHorizontal: 20,
+    marginTop: 15,
+  },
+  button: {
+    backgroundColor: color.primary,
+    paddingVertical: 16,
+    borderRadius: 4,
+    marginBottom: 30,
+    marginHorizontal: 10,
+  },
+  joinTextBottom: {
+    textAlign: 'center',
+    lineHeight: 23,
+  },
+  // -------------------------
+  footer: {
+    marginVertical: 30,
+    marginHorizontal: 20,
+  },
+  logoImage: {
+    resizeMode: 'contain',
+    width: 141.71,
+  },
+  footerText: {
+    lineHeight: 20,
+  },
+  footerSection: {
+    marginTop: 38,
+    marginRight: 30,
+  },
+  sectionHeader: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#000000',
+  },
+  sectionItem: {
+    color: color.textLabel,
+    marginTop: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  sectionLogo1: {
+    resizeMode: 'contain',
+    width: 80.3,
+    height: 29.52,
+  },
+  sectionLogo2: {
+    resizeMode: 'contain',
+    width: 105.57,
+    height: 16.25,
+    marginTop: 10,
+  },
+  sectionLogo3: {
+    resizeMode: 'contain',
+    width: 76.24,
+    height: 23.78,
+    marginTop: 5,
+  },
+  sectionVector: {
+    color: color.textLabel,
+    marginTop: 15,
+    flexDirection: 'row',
+  },
+  sectionItemVector: {
+    marginRight: 45,
+  },
+  footerEnd: {
+    color: color.textLabel,
+    marginTop: 50,
+    marginBottom: 30,
   },
 });
