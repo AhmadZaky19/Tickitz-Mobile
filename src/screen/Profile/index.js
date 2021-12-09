151bea662d7134a7337b31cfc3ad0f013dbffcda
AhmadZaky19 <a.zaky32@gmail.com>
2021-12-10 01:57:07 +0700

    add profile screen (slicing)

--- src/screen/Profile/index.js
+++ src/screen/Profile/index.js
@@ -1,44 +1,59 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  Button,
-  ScrollView,
-  Image,
-  TextInput,
-  TouchableOpacity,
-} from 'react-native';
-import styles from './style';
-import {Icon} from 'react-native-vector-icons/Feather';
+import {useWindowDimensions, Text, ScrollView} from 'react-native';
+import {TabView, SceneMap, TabBar} from 'react-native-tab-view';
 
-function Profile(props) {
+import DetailsAccount from '../../components/DetailsAccount';
+import Footer from '../../components/Footer';
+
+import styles from './style';
+
+const FirstRoute = () => (
+  <ScrollView contentContainerStyle={styles.container}>
+    <DetailsAccount />
+    <Footer />
+  </ScrollView>
+);
+
+const SecondRoute = () => (
+  <ScrollView contentContainerStyle={styles.container}>
+    <Text>hello world 2</Text>
+    <Footer />
+  </ScrollView>
+);
+
+const renderScene = SceneMap({
+  first: FirstRoute,
+  second: SecondRoute,
+});
+
+export default function TabViewExample() {
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    {key: 'first', title: 'Details Account'},
+    {key: 'second', title: 'Order History'},
+  ]);
+  const renderTabBar = props => (
+    <TabBar
+      {...props}
+      renderLabel={({focused, route}) => (
+        <Text style={{color: focused ? '#14142B' : '#AAAAAA'}}>
+          {route.title}
+        </Text>
+      )}
+      indicatorStyle={styles.tabBorder}
+      style={styles.tab}
+    />
+  );
+
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <View style={styles.header}>
-        <View style={styles.headerText}>
-          <Text>Hello,</Text>
-          <Text>Jonathan!</Text>
-        </View>
-        <View style={styles.headerImage}>
-          <Image
-            source={require('../../assets/unsri.png')}
-            style={styles.headerImageSize}
-          />
-        </View>
-      </View>
-      <View style={styles.search}>
-        <View style={styles.searchInput}>
-          <Icon name="search" size={25} styles={styles.searchIcon} />
-          <TextInput styles={styles.searchInputText} />
-        </View>
-        <View style={styles.sectionButton}>
-          <TouchableOpacity style={styles.buttonFilter}>
-            <Text>F</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </ScrollView>
+    <TabView
+      navigationState={{index, routes}}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{width: layout.width}}
+      renderTabBar={renderTabBar}
+    />
   );
 }
-
-export default Profile;
--- src/screen/Profile/style.js
+++ src/screen/Profile/style.js
@@ -1,56 +1,14 @@
 import {StyleSheet} from 'react-native';
+import * as color from '../../styles/colorStyles';
 
 export default StyleSheet.create({
   container: {
-    marginHorizontal: 10,
+    backgroundColor: color.white,
   },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+  tabBorder: {
+    backgroundColor: color.primary,
   },
-  headerText: {},
-  headerImage: {
-    width: 50,
-    height: 50,
-    elevation: 10,
-  },
-  headerImageSize: {
-    width: 50,
-    height: '100%',
-    borderRadius: 50,
-    borderColor: 'black',
-    borderWidth: 1,
-  },
-  search: {
-    marginTop: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  searchInput: {
-    flex: 4,
-    height: 40,
-    backgroundColor: 'white',
-    borderRadius: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
-    elevation: 5,
-  },
-  searchIcon: {
-    marginLeft: 10,
-  },
-  sectionButton: {
-    flex: 1,
-    alignItems: 'flex-end',
-  },
-  searchInputText: {
-    flex: 1,
-  },
-  buttonFilter: {
-    backgroundColor: 'blue',
-    width: 40,
-    height: 40,
-    borderRadius: 10,
+  tab: {
+    backgroundColor: color.white,
   },
 });
