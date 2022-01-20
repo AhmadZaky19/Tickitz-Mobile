bbdbcd23625d72cd05e960f385ffc319fffb93f2
AhmadZaky19 <a.zaky32@gmail.com>
2022-01-20 17:46:47 +0700

    add integration to order history

--- src/components/orderHistory.js
+++ src/components/orderHistory.js
@@ -1,39 +1,74 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import moment from 'moment';
+import {useDispatch, useSelector} from 'react-redux';
 import {Text, View, StyleSheet, Image} from 'react-native';
 import {Button, Card} from 'react-native-elements';
+import {getDataBookingByUserId} from '../stores/actions/booking';
 import * as color from '../styles/colorStyles';
 
+import ebvid from '../assets/img/ebuid.png';
+import cineone from '../assets/img/cineone.png';
+import hiflix from '../assets/img/hiflix.png';
+
 function OrderHistory() {
+  const dispatch = useDispatch();
+  const authData = useSelector(state => state.auth);
+  const orderData = useSelector(state => state.booking);
+  // console.log(orderData);
+
+  const [data, setData] = useState(orderData.data);
+  console.log(data);
+
+  const getBookingByUserId = () => {
+    dispatch(getDataBookingByUserId(authData.idUser)).then(res => {
+      setData(res.value.data.data);
+    });
+  };
+
+  useEffect(() => {
+    getBookingByUserId();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Card containerStyle={styles.info}>
-        <View style={styles.order}>
-          <View style={styles.image}>
-            <Image
-              source={require('../assets/img/cineone.png')}
-              style={styles.cinemaImage}
-            />
-          </View>
-          <Text style={styles.date}>Tuesday, 07 July 2020 - 04:30pm</Text>
-          <Text style={styles.movie}>The Lion King</Text>
-        </View>
-        <Card.Divider />
-        <Button title="Ticket in active" buttonStyle={styles.button1} />
-      </Card>
-      <Card containerStyle={styles.info}>
-        <View style={styles.order}>
-          <View style={styles.image}>
-            <Image
-              source={require('../assets/img/ebuid.png')}
-              style={styles.cinemaImage}
-            />
-          </View>
-          <Text style={styles.date}>Tuesday, 07 July 2020 - 04:30pm</Text>
-          <Text style={styles.movie}>Spider-man: Homecoming</Text>
-        </View>
-        <Card.Divider />
-        <Button title="Ticket used" buttonStyle={styles.button2} />
-      </Card>
+      {data.length > 0 ? (
+        data.map(item => (
+          <Card containerStyle={styles.info} key={item.id}>
+            <View style={styles.order}>
+              <View style={styles.image}>
+                <Image
+                  source={
+                    item.premiere === 'ebu.id'
+                      ? ebvid
+                      : item.premiere === 'hiflix'
+                      ? hiflix
+                      : cineone
+                  }
+                  style={styles.cinemaImage}
+                />
+              </View>
+              <Text style={styles.date}>
+                {moment(item.dateBooking).format('dddd, MMMM Do YYYY')} -
+                {item.timeBooking.substring(0, 5)}
+              </Text>
+              <Text style={styles.movie}>{item.name}</Text>
+            </View>
+            <Card.Divider />
+            {item.statusUsed === 'active' ? (
+              <Button title="Ticket in active" buttonStyle={styles.button1} />
+            ) : (
+              <Button
+                title="Ticket used"
+                disabledTitleStyle={{color: 'white'}}
+                disabledStyle={styles.button2}
+                disabled
+              />
+            )}
+          </Card>
+        ))
+      ) : (
+        <Text style={styles.noBooking}>No order history</Text>
+      )}
     </View>
   );
 }
@@ -52,20 +87,19 @@ const styles = StyleSheet.create({
   },
   order: {
     marginHorizontal: 30,
-    marginTop: 20,
-    marginBottom: 40,
+    marginVertical: 20,
   },
-  image: {},
   cinemaImage: {
     resizeMode: 'contain',
-    width: 160,
+    height: 40,
+    width: 120,
   },
   date: {
-    marginTop: 10,
+    marginTop: 20,
   },
   movie: {
     marginTop: 10,
-    fontSize: 20,
+    fontSize: 22,
     fontWeight: '600',
     color: '#000000',
   },
@@ -82,6 +116,13 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     marginTop: 20,
     marginHorizontal: 30,
+  },
+  noBooking: {
+    marginTop: 30,
+    fontWeight: '700',
+    fontSize: 30,
+    color: color.primary,
+    textAlign: 'center',
   },
 });
 
